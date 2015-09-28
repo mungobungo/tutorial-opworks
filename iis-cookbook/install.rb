4131fb6834400e809aadab56770039c96606d918
Oleg Taykalo <oleg.taykalo@gmail.com>
2015-09-28 22:49:07 +0300

    simple iis enabler

---
+++ iis-cookbook/install.rb
@@ -0,0 +1,9 @@
+#
+wershell_script 'Install IIS' do
+  code 'Add-WindowsFeature Web-Server'
+  not_if "(Get-WindowsFeature -Name Web-Server).Installed"
+end
+
+service 'w3svc' do
+  action [:start, :enable]
+end

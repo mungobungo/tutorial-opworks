3d5aff3dc255c57180679502f949e941d5552372
Oleg Taykalo <oleg.taykalo@gmail.com>
2015-09-29 00:29:41 +0300

    added test file deploy

---
+++ iis-cookbook/recipes/deploy.rb
@@ -0,0 +1,20 @@
+chef_gem "aws-sdk" do
+  compile_time false
+  action :install
+end
+
+chef_gem "git" do
+  compile_time false
+  action :install
+end
+
+ruby_block "download-object" do
+  block do
+    require 'git'
+    URI = 'https://github.com/mungobungo/tutorial-opworks.git'
+    NAME = 'tutorial-opworks'
+    g = Git.clone(URI, NAME, :path => 'c:/temp')
+    FileUtils.cp_r(Dir['c:\temp\site\*'],'c:/inetpub/wwwroot/')
+  end
+  action :run
+end

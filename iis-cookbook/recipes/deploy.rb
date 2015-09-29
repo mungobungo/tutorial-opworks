8168a812beeabeb97f5a159d125d770ad9e953cb
Oleg Taykalo <Oleg.Taykalo@albumprinter.com>
2015-09-29 14:49:34 +0200

    changed simpe git recipe

--- iis-cookbook/recipes/deploy.rb
+++ iis-cookbook/recipes/deploy.rb
@@ -1,24 +1,16 @@
-chef_gem "aws-sdk" do
-  compile_time false
-  action :install
-end
+# chef_gem "aws-sdk" do
+#   compile_time false
+#   action :install
+# end
 
 chef_gem "git" do
   compile_time false
   action :install
 end
 
-ruby_block "download-object" do
-  block do
-   require 'git'
-    URI = 'https://github.com/mungobungo/tutorial-opworks.git'
-    NAME = 'tutorialop'
-    g = Git.clone(URI, NAME, :path => 'c:/temp')
-  end
-  action :run
-end
-
-cookbook_file "c:/inetpub/wwwroot/default.htm" do
-  source "c:/temp/tutorialop/site/default.htm"
+git 'c:/inetpub/wwwroot/' do
+  repository 'https://github.com/mungobungo/aws-test-iis-site.git'
+  revision 'master'
+  action :sync
 end
 

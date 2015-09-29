b2016387aef90ba88222ef282bc2b04c545bec6c
Oleg Taykalo <Oleg.Taykalo@albumprinter.com>
2015-09-29 13:37:31 +0200

    replaced file cook

--- iis-cookbook/recipes/deploy.rb
+++ iis-cookbook/recipes/deploy.rb
@@ -8,13 +8,8 @@ chef_gem "git" do
   action :install
 end
 
-ruby_block "download-object" do
-  block do
-#    require 'git'
-#    URI = 'https://github.com/mungobungo/tutorial-opworks.git'
-#    NAME = 'tutorial-opworks'
-#    g = Git.clone(URI, NAME, :path => 'c:/temp')
-    FileUtils.cp_r(Dir['c:\temp\tutorial-opworks\site\*'],'c:/inetpub/wwwroot/')
-  end
-  action :run
+cookbook_file "c:/inetpub/wwwroot/default.htm" do
+  source "c:/temp/tutorial-opworks/site/default.htm"
+  action :create
 end
+
